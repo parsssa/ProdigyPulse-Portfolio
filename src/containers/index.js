2a7bd265224e85d03188c78e74527fcdbd6d5168
Parsa Bagheri <107321532+parsssa@users.noreply.github.com>
2023-03-29 09:56:01 +0200

    better media queries. photo imports don't work in Skillset

--- src/containers/index.js
+++ src/containers/index.js
@@ -6,3 +6,18 @@ export {default as Projects} from './projects/Projects';
 export {default as Quests} from './quests/Quests';
 export {default as Title} from './title/Title';
 export {default as Skillset} from './skillset/Skillset';
+
+import Telegram from '../assets/Telegram.jpg';
+import figma from '../assets/figma.jpg';
+import MS from '../assets/MS.jpg';
+import FrontEnd from '../assets/FrontEnd.jpg';
+import linkedin from '../assets/linkedin.jpg';
+import python from '../assets/python.jpg';
+import Maven from '../assets/Maven.jpg';
+import google from '../assets/google.jpg';
+import java from '../assets/java.jpg';
+import JavaFx from '../assets/JavaFx.jpg';
+import translation from '../assets/translation.jpg';
+import react from '../assets/react.jpg';
+
+export {Telegram,figma,MS,FrontEnd,linkedin,python,Maven,google,java,JavaFx,translation,react};
--- src/containers/skillset/Skillset.jsx
+++ src/containers/skillset/Skillset.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
-import './skillset.css'
+import './skillset.css';
+import {Telegram,figma,MS,Front,linkedin,python,Maven,google,java,JavaFx,translation,react} from '../index.js';
+
 
 const Skillset = () => {
   return (
-    <div>Skillset</div>
+    <div className='skillset-area'>
+      <h1>Parsa Bagheri</h1>
+      <div className='skillset-box'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed id enim tempor, pulvinar enim in, mollis turpis. Integer sagittis ante sed ante volutpat hendrerit. Fusce convallis lorem vel arcu aliquet, eget accumsan metus vehicula. Quisque auctor sagittis arcu, vel eleifend mi fringilla in. Sed vitae semper nulla. Aliquam sit amet
+          fringilla, tortor ut ullamcorper hendrerit, lacus risus aliquet risus, a luctus eros justo et mauris.</p>
+        <img src={Telegram} alt="Me at my piano" />
+      </div>
+    </div>
   )
 }
 
