d4b5b53d5d64880097cd23db1fac7cea5476e3ad
Parsa Bagheri <107321532+parsssa@users.noreply.github.com>
2023-03-29 11:27:18 +0200

    updated Skill, updated Skillset, fixed Navbar, fixed imports

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
+import Telegram from '../assets/Telegram.jpg';
 import React from 'react';
 import ReactDOM from 'react-dom';
 export {default as Skill} from './skill/Skill';
 export {default as Connection_item} from './connection_item/Connection_item';
 export {default as Transparent_rectangle} from './transparent_rectangle/Transparent_rectangle';
+
+export {
+ Telegram,
+};
 
 
--- src/containers/index.js
+++ src/containers/index.js
@@ -1,23 +1,36 @@
-export {default as Bio} from './bio/Bio';
-export {default as Connections} from './connections/Connections';
-export {default as Footer} from './footer/Footer';
-export {default as Navbar} from './navbar/Navbar';
-export {default as Projects} from './projects/Projects';
-export {default as Quests} from './quests/Quests';
-export {default as Title} from './title/Title';
-export {default as Skillset} from './skillset/Skillset';
-
 import Telegram from '../assets/Telegram.jpg';
-import figma from '../assets/figma.jpg';
-import MS from '../assets/MS.jpg';
+import figma from '../assets/figma.png';
+import MS from '../assets/MS.png';
 import FrontEnd from '../assets/FrontEnd.jpg';
-import linkedin from '../assets/linkedin.jpg';
-import python from '../assets/python.jpg';
-import Maven from '../assets/Maven.jpg';
-import google from '../assets/google.jpg';
-import java from '../assets/java.jpg';
-import JavaFx from '../assets/JavaFx.jpg';
-import translation from '../assets/translation.jpg';
-import react from '../assets/react.jpg';
+import linkedin from '../assets/linkedin.png';
+import python from '../assets/python.png';
+import Maven from '../assets/Maven.png';
+import google from '../assets/google.png';
+import java from '../assets/java.png';
+import JavaFx from '../assets/JavaFX.png';
+import translation from '../assets/translation.png';
+import react from '../assets/react.png';
 
-export {Telegram,figma,MS,FrontEnd,linkedin,python,Maven,google,java,JavaFx,translation,react};
+export {
+  Telegram,
+  figma,
+  MS,
+  FrontEnd,
+  linkedin,
+  python,
+  Maven,
+  google,
+  java,
+  JavaFx,
+  translation,
+  react,
+};
+
+export { default as Bio } from './bio/Bio';
+export { default as Connections } from './connections/Connections';
+export { default as Footer } from './footer/Footer';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Projects } from './projects/Projects';
+export { default as Quests } from './quests/Quests';
+export { default as Title } from './title/Title';
+export { default as Skillset } from './skillset/Skillset';
